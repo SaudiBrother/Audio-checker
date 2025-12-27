63b1858acd8e61646175b10e5ba9a722fa522b1d
SaudiBrother <rynxxchan@gmail.com>
2025-12-27 10:12:26 +0800

    Delete assets/js directory

--- assets/js/modules/audio-worker.js
+++
@@ -1,9 +0,0 @@
-self.onmessage = function(e) {
-    if (e.data.command === 'start') {
-        console.log("Worker: Processing Audio...");
-        // Logika pemrosesan data audio mentah di sini
-    } else if (e.data.command === 'stop') {
-        console.log("Worker: Saving File...");
-        // Logika bungkus ke WAV/MP3
-    }
-};
--- assets/js/modules/config.js
+++
@@ -1,5 +0,0 @@
-export const CONFIG = {
-    sampleRate: 44100,
-    bufferSize: 4096,
-    visualizerColor: '#3498db'
-};
