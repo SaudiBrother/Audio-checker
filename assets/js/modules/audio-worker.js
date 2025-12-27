887c3d7fee0a821029eff3866d0ed3e76c02ca08
SaudiBrother <rynxxchan@gmail.com>
2025-12-27 10:06:25 +0800

    Implement audio processing commands in worker
    Added message handling for audio processing commands.

--- assets/js/modules/audio-worker.js
+++ assets/js/modules/audio-worker.js
@@ -1,1 +1,9 @@
-
+self.onmessage = function(e) {
+    if (e.data.command === 'start') {
+        console.log("Worker: Processing Audio...");
+        // Logika pemrosesan data audio mentah di sini
+    } else if (e.data.command === 'stop') {
+        console.log("Worker: Saving File...");
+        // Logika bungkus ke WAV/MP3
+    }
+};
